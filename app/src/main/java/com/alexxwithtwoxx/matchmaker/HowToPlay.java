8678f36625869957d2ca6d468821108bbb2fb872
Leslie Maan <39062309+Lmaan@users.noreply.github.com>
2018-05-25 16:13:43 -0400

    how to has back button

--- app/src/main/java/com/alexxwithtwoxx/matchmaker/HowToPlay.java
+++ app/src/main/java/com/alexxwithtwoxx/matchmaker/HowToPlay.java
@@ -1,13 +1,31 @@
 package com.alexxwithtwoxx.matchmaker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class HowToPlay extends AppCompatActivity {
+
+    Button buttonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to_play);
+
+        buttonBack = findViewById(R.id.buttonBack);
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HowToPlay.this, MenuActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
