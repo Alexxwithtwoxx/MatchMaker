7a78cd5f829d1be79db68bb2e8f5b915bf704580
Luke Malcolm <LMalcolm25@gmail.com>
2018-05-25 15:31:56 -0400

    Menu Works
    Play button works

---
+++ app/src/main/java/com/alexxwithtwoxx/matchmaker/MenuActivity.java
@@ -0,0 +1,36 @@
+package com.alexxwithtwoxx.matchmaker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
+
+public class MenuActivity extends AppCompatActivity {
+
+    protected ImageButton buttonPlay;
+
+    protected ImageButton button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+        buttonPlay = findViewById(R.id.buttonPlay);
+
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+    }
+}
